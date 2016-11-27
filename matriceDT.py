8fd447c82058685810db6da1fab506144c7fecf0
Arkwith <matthieu.drouaire@imerir.com>
2016-11-27 21:43:28 +0100

    MAJ parser matrice
    pas encore de fonction return

--- matriceDT.py
+++ matriceDT.py
@@ -17,6 +17,8 @@ class matriceDT:
                         if (item[0] != "T"):
                             value = [item, str(int(item) * 60 / 25)]
                             line.append(value)
+                        else:
+                            line.append(item)
                 self.matrice.append(line)
         finally:
             f.close()
@@ -28,12 +30,37 @@ class matriceDT:
         try:
             readerf = csv.reader(f, delimiter = ";")
             readerg = csv.reader(g, delimiter = ",")
-            for row in 
+            for row in readerf:
+                line = []
+                for item in row:
+                    if (item != ""):
+                        if (item[0] != "T"):
+                            value = [item, "empty"]
+                            line.append(value)
+                        else:
+                            line.append(item)
+                self.matrice.append(line)
+
+            i = 0
+            for row in readerg:
+                j = -1
+                for item in row:
+                    if (item != ""):
+                        if (item[0] != "T"):
+                            if(i != 0):
+                                print(item, j, i)
+                                #self.matrice[i][j][1] = item
+                            
+                    j = j+1
+                i = i+1
+        finally:
+            f.close()
+            g.close()
 
 
 
 mat = matriceDT()
-mat.initD("dist_terminus.csv")
+mat.initD("dist_terminus.csv")#, "terminus.csv")
 print(mat.matrice)
 
 #g = open("terminus.csv", 'rb')
