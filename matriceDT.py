3cbe765782a7eeb4473af48e48a4026a651d4545
Arkwith <matthieu.drouaire@imerir.com>
2016-11-28 09:31:13 +0100

    matriceDT
    getDistance('T','T)
    getDuree('T','T)

--- matriceDT.py
+++ matriceDT.py
@@ -9,7 +9,7 @@ class matriceDT:
 
         f = open(distanceFichier, 'rb')
         try:
-            reader = csv.reader(f, delimiter = ";")
+            reader = csv.reader(f, delimiter = ",")
             for row in reader:
                 line = []
                 for item in row:
@@ -28,45 +28,46 @@ class matriceDT:
         g = open(tempsFichier, 'rb')
 
         try:
-            readerf = csv.reader(f, delimiter = ";")
-            readerg = csv.reader(g, delimiter = ",")
-            for row in readerf:
+            readerDistance = csv.reader(f, delimiter = ",")
+            readerDuree = csv.reader(g, delimiter = ",")
+            for row in readerDistance:
                 line = []
                 for item in row:
                     if (item != ""):
-                        if (item[0] != "T"):
-                            value = [item, "empty"]
+                        if ("T" not in item):
+                            value = [item, 0]
                             line.append(value)
                         else:
                             line.append(item)
                 self.matrice.append(line)
 
             i = 0
-            for row in readerg:
-                j = -1
+            for row in readerDuree:
+                j = 0
                 for item in row:
                     if (item != ""):
-                        if (item[0] != "T"):
-                            if(i != 0):
-                                print(item, j, i)
-                                #self.matrice[i][j][1] = item
-                            
+                        if ("T" not in item):
+                            self.matrice[i][j][1] = item
                     j = j+1
                 i = i+1
         finally:
             f.close()
             g.close()
 
+    def getDistance(self, ligne, colonne):
+        i = int(ligne[1:])
+        j = int(colonne[1:])
+        print(i,j)
+        return self.matrice[i+1][j+1][0]
+
+    def getDuree(self, ligne, colonne):
+        i = int(ligne[1:])
+        j = int(colonne[1:])
+        return self.matrice[i+1][j+1][1]
 
 
 mat = matriceDT()
-mat.initD("dist_terminus.csv")#, "terminus.csv")
-print(mat.matrice)
+mat.initDT("dist_terminus.csv", "terminus.csv")
 
-#g = open("terminus.csv", 'rb')
-#try:
-#    reader = csv.reader(g, delimiter = ",")
-#    for row in reader:
-#        print row.value
-#finally:
-#    g.close()
+print(mat.getDistance('T5','T12'), mat.getDuree('T5','T12'))
+
