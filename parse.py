13129224b3d4459f355784becd6904becaafc456
Ahmed El Moden <ahmed.elmoden@imerir.com>
2016-11-28 16:40:50 +0100

    Ajout d'un option de tri chronologique par ligne et sur l'ensemble

--- parse.py
+++ parse.py
@@ -4,10 +4,20 @@ import time, sys, getopt
 import csv
 import operator
 from datetime import timedelta
+from solution import *
 
 
-def parse(horaireFile):
+def initSolution(lignes, indexLignes):
+    s = Solution(lignes, indexLignes)
+    return s
+
+def parse(horaireFile, sortedByHDepart=True):
     listTrajets = []
+    tempListTrajets = []
+    previousLigne = None
+    lignes = {}
+    indexLignes = []
+    isLigneDifferent = False
     with open(horaireFile, 'r') as f:
         csvFile = csv.reader(f, delimiter=',')
         numLigne = ""
@@ -22,7 +32,13 @@ def parse(horaireFile):
                 col = row[iCol]
                 if "ligne" in col:
                     sens = "a" if sens == "r" else "r"
-                    numLigne = col[5:]
+                    numLigne = col[6:]
+                    if previousLigne != numLigne and previousLigne != None:
+                        isLigneDifferent = True
+                    else:
+                        isLigneDifferent = False
+
+                    previousLigne = numLigne
                     noRow = True
                     break
                 elif iCol == 0 and "Dist" not in col:
@@ -37,7 +53,8 @@ def parse(horaireFile):
             if len(rowList) != 0 and noRow == True:
                 # Parse rows
                 maxTrajetRowSize = getMaxTrajetRowSize(rowList)
-                # print "RowList : ", len(rowList)
+                lignes[numLigne + ":" + sens] = [None for _ in range(maxTrajetRowSize)]
+                indexLignes.append(numLigne + ":" + sens)
 
                 for x in range(1, maxTrajetRowSize):
                     firstTime = None
@@ -59,17 +76,55 @@ def parse(horaireFile):
                         except IndexError:
                             # print "Index Error"
                             continue
+
                     if firstTime != None and lastTime != None:
                         hDepart = timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=int(firstTime.split(":")[1]), hours=int(firstTime.split(":")[0]))
                         hArrivee = timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=int(lastTime.split(":")[1]), hours=int(lastTime.split(":")[0]))
                         t = Trajet(hDepart, hArrivee, tDepart, tArrivee, dist, "l"+numLigne, sens, x, hArrivee - hDepart)
-                        print(hDepart, hArrivee, tDepart, tArrivee, dist, "l"+numLigne, sens, x, hArrivee - hDepart)
-                        listTrajets.append(t)
+
+                        tempListTrajets.append(t)
+
+                        if sortedByHDepart == False:
+                            # print previousLigne, " " , numLigne
+                            tempLastTrajet = tempListTrajets[-1]
+                            del tempListTrajets[-1]
+
+                            if isLigneDifferent == True:
+                                tempListTrajets = sorted(tempListTrajets, key=operator.attrgetter("hDepart"))
+                                listTrajets.extend(tempListTrajets)
+                                del tempListTrajets[:]
+                                tempListTrajets.append(tempLastTrajet)
+                            else:
+                                tempListTrajets.append(t)
+                        else:
+                            listTrajets.append(t)
+
+                        # print(hDepart, hArrivee, tDepart, tArrivee, dist, "l"+numLigne, sens, x, hArrivee - hDepart)
 
                 noRow = False
                 rowList = []
-    listTrajets = sorted(listTrajets, key=operator.attrgetter("hDepart"))
-    return listTrajets
+            previousLigne = numLigne
+    s = initSolution(indexLignes, lignes)
+
+    # i = 1
+    # for index in indexLignes:
+    #     print index + " " + str(lignes[index])
+
+    #     if i % 2 == 0:
+    #         print ""
+
+    #     i += 1
+
+    for t in listTrajets:
+        print t
+    # print listTrajets
+
+
+    if sortedByHDepart == True:
+        listTrajets = sorted(listTrajets, key=operator.attrgetter("hDepart"))
+
+    s = initSolution(lignes, indexLignes)
+    return (listTrajets, s)
 
 def getMaxTrajetRowSize(rowList):
     maxRowSize = 0
@@ -108,7 +163,7 @@ class Trajet:
         return text
 
 
-'''
+# '''
 horaireFile = ""
 try:
     opts, args = getopt.getopt(sys.argv[1:],"h:")
@@ -120,5 +175,5 @@ for opt, arg in opts:
     if opt == '-h':
         horaireFile = arg
 
-parse(horaireFile)
-'''
+parse(horaireFile, False)
+# '''
---
+++ solution.py
@@ -0,0 +1,13 @@
+class Solution:
+        lignes = None
+        indexLignes = []
+	def __init__(self, lignes, indexLignes):
+		self.matBus = []
+                self.lignes = lignes
+                self.indexLignes = indexLignes
+
+        def initNbLignes(self, nb):
+            lignes = [] *  nb
+
+
+
