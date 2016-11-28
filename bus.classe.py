4fd7066cbe07534758cfda11269ae76bc7f1c709
Wasoh <wasoh@alex.pujolas@gmail.com>
2016-11-28 09:26:12 +0100

    ajout fonction addTrajet

--- bus.classe.py
+++ bus.classe.py
@@ -1,10 +1,19 @@
 class Bus:
     def __init__(self, num, hDep, tDep):
-        trajet = new Trajet(0, hDep, 'T0', tDep)
+        trajet = new Trajet(0, 
+                            hDep, 
+                            'T0', 
+                            tDep, 
+                            distance("T0", tDep), 
+                            0, 
+                            "", 
+                            "", 
+                            duree("T0", tDep))
         self.num = num
         self.hSor = trajet.duree
         self.hRet = -1
         self.planning = []
+        self.planning.append(trajet)
 
     def dispo(heure, terminus):
         lastTrajet = self.planning[-1]
@@ -26,4 +35,32 @@ class Bus:
         return res
 
     def addTrajet(trajet):
-        #TODO
+        lastTrajet = self.planning[-1]
+        dure = duree(lastTrajet.tArrivee, trajet.tArrivee)
+        trajetTerminus = new Trajet(lastTrajet.hArrivee, 
+                            trajet.hDepart, 
+                            lastTrajet.tArrivee, 
+                            trajet.tDepart, 
+                            distance(lastTrajet.tArrivee, trajet.tDepart), 
+                            0, 
+                            "", 
+                            "", 
+                            duree(lastTrajet.tArrivee, trajet.tDepart))
+        
+        if(dure < 5):
+            trajetWait = new Trajet(lastTrajet.hArrivee, 
+                                    lastTrajet.hArrivee + (trajet.hDepart - lastTrajet.hArrivee), 
+                                    lastTrajet.tArrivee, 
+                                    lastTrajet.tArrivee, 
+                                    0, 
+                                    0, 
+                                    "", 
+                                    "", 
+                                    trajet.hDepart - lastTrajet.hArrivee)
+            self.planning.append(trajetWait)
+            self.planning.append(trajetTerminus)
+        else if(dure >= 5):
+            self.planning.append(trajetTerminus)
+
+
+        self.planning.append(trajet)
