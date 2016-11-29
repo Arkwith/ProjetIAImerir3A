b23811bbe4daba76f0fbb64a2135f68cd439a74f
Bastien <bastien.philippe@imerir.com>
2016-11-29 16:17:59 +0100

    oui

--- formatSol.py
+++ formatSol.py
@@ -1,13 +1,45 @@
 from bus import *
+from evaluation import *
 
-def format(list_Bus):
+def format(list_Bus,inter):
 	f = open("output","w")
 	text = "# Alex Pujolas, Matthieu Drouaire, Ahmed El Moden, Julien Pozzera, Bastien Philippe\n"
-	text += "n,t,k\n"
-	for b in list_Bus:
-		text += "bus" + str(b.num)
-		for t in b.planning:
-			if(t.ligne[0] == 'l'):
-				text += "," + t.ligne + ":" + t.sens + ":v" + str(t.index)
-		text += "\n"
+	if(inter):
+		n,t,k = scoresUnitaires(list_Bus)
+		text += str(n) + "," + str(t) + "," + str(k) + "\n"
+		for b in list_Bus:
+			text += "bus" + str(b.num) + "," + b.planning[0].tDepart
+			for t in b.planning:
+				if(t.ligne[0] == 'l'):
+					text += "," + t.ligne + ":" + t.sens + ":v" + str(t.index)
+			text += "\n"
+	else:
+		list_ligne =[]
+		ligneMemo = 'l00000000'
+		for b in list_Bus:
+			if(b.planning[1].ligne != ligneMemo):
+				ligneMemo = b.planning[1].ligne
+				if(len(list_ligne) > 0):
+					n,t,k = scoresUnitaires(list_ligne)
+					text += str(n) + "," + str(t) + "," + str(k) + "\n"
+					for bl in list_ligne:
+						text += "bus" + str(bl.num) + "," + bl.planning[0].tDepart
+						for t in bl.planning:
+							if(t.ligne[0] == 'l'):
+								text += "," + t.ligne + ":" + t.sens + ":v" + str(t.index)
+						text += "\n"
+
+				list_ligne =[]
+			list_ligne.append(b)
+		n,t,k = scoresUnitaires(list_ligne)
+		text += str(n) + "," + str(t) + "," + str(k) + "\n"
+		for bl in list_ligne:
+			text += "bus" + str(bl.num) + "," + bl.planning[0].tDepart
+			for t in bl.planning:
+				if(t.ligne[0] == 'l'):
+					text += "," + t.ligne + ":" + t.sens + ":v" + str(t.index)
+			text += "\n"
+
+		
+
 	f.write(text)
--- test.py
+++ test.py
@@ -1,22 +1,7 @@
 from matriceDT import *
 from generator import *
+from formatSol import *
 
 
 x,y = generate(True)
-
-print y.lignes
-
-
-x,y = generate(False)
-
-print y.lignes
-cpt = 0
-for k in y.lignes:
-	print k +"\t"+ str(len(y.lignes[k]))
-	cpt += len(y.lignes[k])
-print cpt
-cpt = 0
-for b in x:
-	for t in b.planning:
-		if(t.ligne[0] == 'l') : cpt += 1
-print cpt
+format(x,True)
