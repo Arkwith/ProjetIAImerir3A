988dbe10c22ab426a3d77d248a1957b5a6fbf325
Wasoh <wasoh@alex.pujolas@gmail.com>
2016-11-29 11:21:15 +0100

    correction

---
+++ evaluation.py
@@ -0,0 +1,24 @@
+from bus import *
+from datetime import timedelta
+
+def scoresUnitaires(listeBus):
+	dureeTot = 0
+	distanceTot = 0
+	nbBus = len(listeBus)
+
+	for bus in listeBus:
+		for trajet in bus.planning:
+			dureeTot += int(trajet.duree.minutes)
+			distanceTot += int(trajet.dist)
+
+	return (nbBus,dureeTot,distanceTot)
+
+def scoreSolution(listeBus):
+	poidBus = 50 
+	poidTemps = 10
+	poidDistance = 10
+
+	nbBus,dureeTot,distanceTot =	scoresUnitaires(listeBus)
+
+	res = (poidBus * nbBus) + (poidTemps * dureeTot) + (poidDistance * distanceTot) 
+	return res
