a89ca4deec897fa8f688beba76a53218c1c913f8
Bastien <bastien.philippe@imerir.com>
2016-11-29 09:46:03 +0100

    9h46

--- formatSol.py
+++ formatSol.py
@@ -1,12 +1,13 @@
 from bus import *
 
 def format(list_Bus):
-	#f = open("output","w")
+	f = open("output","w")
 	text = "# Alex Pujolas, Matthieu Drouaire, Ahmed El Moden, Julien Pozzera, Bastien Philippe\n"
 	text += "n,t,k\n"
 	for b in list_Bus:
 		text += "bus" + str(b.num)
 		for t in b.planning:
-			text += " ," + t.ligne + ":" + t.sens + ":v" + str(t.index)
+			if(t.ligne[0] == 'l'):
+				text += "," + t.ligne + ":" + t.sens + ":v" + str(t.index)
 		text += "\n"
-	print text
+	f.write(text)
